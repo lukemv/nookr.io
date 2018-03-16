d1d45fa6687e5aec8fff6354cb0ab79a9ad19895
Luke M <luke@vibrato.com.au>
2018-03-17 10:41:37 +1100

    Make two different deployment models

---
+++ nookr_up.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Valid environments are (local|dev|prod)
+env="${1:?Usage: ./run.sh (local|dev|prod)}"
+echo "[INFO] Environment: $env"
+export environment=$env
+
+base_dir=".nookr"
+
+if ! [ -d $base_dir ]; then
+  echo "[INFO] Making directory: $base_dir"
+  mkdir $base_dir
+fi
+
+env_folder="$base_dir/$env"
+mkdir -p "$env_folder"
+
+if [ "$env" != "local" ]; then
+  echo "[INFO] Full build requested"
+
+  echo "[INFO] Building API"
+  docker build -t "nookr_api_${env}" ./api
+
+  echo "[INFO] Building UI"
+  pushd ./ui
+  node ./build/build.js
+  popd
+  docker build -t "nookr_ui_${env}" ./ui
+
+  echo "[INFO] Building Gateway Proxy"
+  docker build -t "nookr_proxy_${env}" ./proxy
+
+
+  echo "[INFO] Building Website"
+  pushd ./docs
+  rm -rf ./node_modules
+  npm install
+  ./node_modules/.bin/harp compile src/ dist/
+  popd
+  docker build -t "nookr_docs_${env}" ./docs
+
+  compose_file="compose_env.yml"
+  docker-compose --file $compose_file up
+else
+  docker-compose --file "compose_local.yml" up
+fi

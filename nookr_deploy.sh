f8389553cbd59bf8a193a3e75693c9a7586e49ff
Luke M <luke@vibrato.com.au>
2018-03-21 19:58:09 +1100

    Reduce image sizes and simplify deployment

--- nookr_deploy.sh
+++ nookr_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
-aws ecs update-service \
-  --task-definition "nookr:$revision" \
-  --service "nookr-uat" \
-  --cluster "nookr"
+aws ecs update-service --cluster "nookr" --service "nookr-uat" --force-new-deployment
+#revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
+#aws ecs update-service \
+#  --task-definition "nookr:$revision" \
+#  --service "nookr-uat" \
+#  --cluster "nookr"
--- nookr_push.sh
+++ nookr_push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 REPO="238164213705.dkr.ecr.ap-southeast-2.amazonaws.com"
+aws ecr get-login --no-include-email --region ap-southeast-2 | sh
 
 docker tag "nookr-ui:latest" "$REPO/nookr-ui:latest"
 docker tag "nookr-api:latest" "$REPO/nookr-api:latest"
 docker tag "nookr-proxy:latest" "$REPO/nookr-proxy:latest"
 
 echo "[INFO] Pushing UI"
-docker push "$repo/nookr-ui:latest"
+docker push "$REPO/nookr-ui:latest"
 echo "[INFO] Pushing API"
-docker push "$repo/nookr-api:latest"
+docker push "$REPO/nookr-api:latest"
 echo "[INFO] Pushing Proxy"
-docker push "$repo/nookr-proxy:latest"
+docker push "$REPO/nookr-proxy:latest"

a9a155c8b44ade04198be3052fda32ebb1176f17
Luke M <luke@vibrato.com.au>
2018-03-21 20:39:26 +1100

    Use git revision to tag images

--- nookr_deploy.sh
+++ nookr_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+TAG=$(git rev-parse --short HEAD)
+sed "s/GITHASH/$TAG/g" task-template.json > task-definition.json
 revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
 aws ecs update-service --cluster "nookr" --service "nookr-uat" --force-new-deployment --task-definition "nookr:$revision" \
--- nookr_push.sh
+++ nookr_push.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+TAG=$(git rev-parse --short HEAD)
 REPO="238164213705.dkr.ecr.ap-southeast-2.amazonaws.com"
 aws ecr get-login --no-include-email --region ap-southeast-2 | sh
 
-docker tag "nookr-ui:latest" "$REPO/nookr-ui:latest"
-docker tag "nookr-api:latest" "$REPO/nookr-api:latest"
-docker tag "nookr-proxy:latest" "$REPO/nookr-proxy:latest"
+docker tag "nookr-ui:latest" "$REPO/nookr-ui:$TAG"
+docker tag "nookr-api:latest" "$REPO/nookr-api:$TAG"
+docker tag "nookr-proxy:latest" "$REPO/nookr-proxy:$TAG"
 
 echo "[INFO] Pushing UI"
-docker push "$REPO/nookr-ui:latest"
+docker push "$REPO/nookr-ui:$TAG"
 echo "[INFO] Pushing API"
-docker push "$REPO/nookr-api:latest"
+docker push "$REPO/nookr-api:$TAG"
 echo "[INFO] Pushing Proxy"
-docker push "$REPO/nookr-proxy:latest"
+docker push "$REPO/nookr-proxy:$TAG"

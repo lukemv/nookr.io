79998dea0bfeb304a5526da1d115240035818fcf
Luke M <luke@vibrato.com.au>
2018-03-21 20:25:27 +1100

    Force update service with new revision

--- nookr_deploy.sh
+++ nookr_deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-aws ecs update-service --cluster "nookr" --service "nookr-uat" --force-new-deployment
-#revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
-#aws ecs update-service \
-#  --task-definition "nookr:$revision" \
-#  --service "nookr-uat" \
-#  --cluster "nookr"
+revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
+aws ecs update-service --cluster "nookr" --service "nookr-uat" --force-new-deployment --task-definition "nookr:$revision" \

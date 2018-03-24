35205f71aee71e863a0a0673352d10bd07674e1b
tobyl001 <toby.ling1@gmail.com>
2018-03-23 21:19:16 -0400

    Initial book list prototype

---
+++ ui/src/components/BookList/BookList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="#book-list">
+    <book-shelf v-bind:books="recommended" v-bind:shelf-title="'Just For You'"></book-shelf>
+    <book-shelf v-bind:books="trending" v-bind:shelf-title="'Trending'"></book-shelf>
+    <book-shelf v-bind:books="newRelease" v-bind:shelf-title="'New Releases'"></book-shelf>
+  </div>
+</template>
+
+<script>
+    import BookShelf from './BookShelf'
+
+    export default {
+      components: {BookShelf},
+      name: 'book-list',
+      data () {
+        return {
+          recommended: [
+            {title: 'Harry Potter', coverImage: 'harry-potter.jpg'},
+            {title: 'Game of Thrones', coverImage: 'game-of-thrones.jpg'}],
+          trending: [
+            {title: 'Hunger Games', coverImage: 'hunger-games.jpg'},
+            {title: 'Lord of the Flies', coverImage: 'lord-of-the-flies.png'}],
+          newRelease: [
+            {title: 'Animal Farm', coverImage: 'animal-farm.jpg'},
+            {title: 'To Kill a Mockingbird', coverImage: 'mockingbird.jpg'}]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>

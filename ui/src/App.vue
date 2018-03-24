ee79ada1102e27835943887f5a9ab188001378cf
Jenna Afarian <jenna@afarian.me>
2018-03-24 23:21:09 +1000

    Add login link

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <nav class="navbar navbar-dark bg-primary">
       <span class="navbar-brand mb-0 h1">nookr.io</span>
+      <span class="navbar-text">
+        <ul class="nav">
+          <li class="ml-1" v-if="!('Login' === $route.name)">
+            <router-link :to="{ name: 'Login' }">Login</router-link>
+          </li>
+        </ul>
+      </span>
     </nav>
     <main>
       <router-view></router-view>
@@ -18,4 +25,3 @@ export default {
 <style lang="sass">
 @import '../bootstrap/scss/bootstrap';
 </style>
-

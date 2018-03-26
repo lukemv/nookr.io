8ec83eb00fc1b6b36ade49455041f495c9686d49
Dallas <s3461243@student.rmit.edu.au>
2018-03-26 21:59:14 +1100

    Initial commit. (#jk - really the fourth commit but I had to start fresh due to access issues.)
    Login.vue - Changed email input type from text to email, so we get basic email valiation for free. Changed the email and password validation message placeholder to match Register's one. Added register button and navigation functionality for it.
    
    Register.vue - Based off of Login.vue, added a secondary password field, added register button with harness post submission.
    
    index.js - Changed a path back to / otherwise when the page loads and there is no / the page will be blank. Added the import statement needed for the named route to work.
    
    App.vue - Removed my router-link for registration, since Gin was doing that. Left in the documentation links that I found.

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
 import BookList from '@/components/BookList/BookList'
 
 Vue.use(Router)
 
 export default new Router({
+  // Named views - https://router.vuejs.org/en/essentials/named-routes.html
+  // Note: One path needs to be / or when the page loads it will be blank.
   routes: [
     {
       path: '/',
@@ -16,6 +19,11 @@ export default new Router({
       path: '/book-list',
       name: 'BookList',
       component: BookList
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })

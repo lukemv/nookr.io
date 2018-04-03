f0bb9f036ac070a7620d5f8e42cbad22d1408736
tobyl001 <toby.ling1@gmail.com>
2018-04-03 15:36:44 -0400

    Updated the booklist to call google books api. All api calls are currently hard coded.

--- ui/src/components/BookList/BookThumbnail.vue
+++ ui/src/components/BookList/BookThumbnail.vue
@@ -1,8 +1,10 @@
-<template>
-  <div >
-    <img class="book-img" v-bind:src="require('../../assets/' + book.coverImage)" alt="Book Cover">
-    <div class="book-title"> {{book.title}}</div>
-  </div>
+ <template>
+   <router-link :to="{ path: 'book', query: { id: book.isbn10 }}">
+    <div class="book-thumb" >
+      <img class="book-img" v-bind:src="book.coverImage" alt="Book Cover">
+      <div class="book-title"> {{book.title}}</div>
+    </div>
+   </router-link>
 </template>
 
 <script>
@@ -13,13 +15,15 @@
 </script>
 
 <style scoped>
+
   .book-img{
     height: 200px;
     width: auto;
   }
   .book-title{
     padding-top: 5px;
-    font-size: 1.1rem;
+    font-size: 1rem;
     width: 200px;
+    color: #666666;
   }
 </style>

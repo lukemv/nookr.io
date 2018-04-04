3db39951a85409931442bb3e0463a7664a056755
Luke M <luke@lukemuccillo.com>
2018-04-04 17:02:22 +1000

    45 - Update task definition with additional redirect image (#22)

--- infra/auxiliary/deploy.sh
+++ infra/auxiliary/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-revision=$(aws ecs register-task-definition --cli-input-json file://task-definition.json | jq  '.taskDefinition.revision')
-sed "s/TASK_DEFINITION/$revision/g" service-template.json > service.json
-aws ecs create-service --cli-input-json file://service.json
+revision=$(aws ecs register-task-definition --cli-input-json "file://task-template.json" | jq  '.taskDefinition.revision')
+sed "s/TASK_DEFINITION/$revision/g" "service-template.json" > "service.json"
+aws ecs create-service --cli-input-json "file://service.json"

5c4bb32879b0e8bf2c1e8691705dd65935901831
Jenna Afarian <jenna.afarian@hotmail.com>
2018-04-16 20:46:21 +1000

    Filter by weeks on list - validation
    Additional validation added for non-numerical entries.

--- ui/src/components/NYBooks.vue
+++ ui/src/components/NYBooks.vue
@@ -38,7 +38,6 @@
                 :disabled="isLoading"
                 class="mb-2 mr-sm-2 pt-1 col-sm-7"
                 id="filterByWeeks"
-                type="number"
                 v-focus>
       </p>
       <button
@@ -99,6 +98,12 @@
     methods: {
       filterBooks: function () {
         this.isLoading = true
+        if (!this.filterDate && !this.weeksOnList) {
+          this.errors = []
+          this.errors.push('Please enter at least one criteria to filter by')
+          this.isLoading = false
+          return
+        }
         if (this.filterDate && !this.weeksOnList) {
           this.errors = []
           if (!this.validDate(this.filterDate)) {
@@ -123,7 +128,7 @@
         }
         if (this.weeksOnList && !this.filterDate) {
           this.errors = []
-          if (this.weeksOnList < 0) {
+          if (!this.validWeeks(this.weeksOnList) || this.weeksOnList < 0) {
             this.errors.push('Please enter a valid number (weeks on best sellers list must be zero or greater)')
             this.isLoading = false
             return
@@ -149,7 +154,7 @@
             this.errors.push('Please enter a valid date (YYYY-MM-DD)')
             this.isLoading = false
           }
-          if (this.weeksOnList < 0) {
+          if (!this.validWeeks(this.weeksOnList) || this.weeksOnList < 0) {
             this.errors.push('Please enter a valid number (weeks on best sellers list must be zero or greater)')
             this.isLoading = false
           }
@@ -185,6 +190,10 @@
       validDate: function (filterDate) {
         var re = /(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))/
         return re.test(filterDate)
+      },
+      validWeeks: function (weeksOnList) {
+        var re = /[0-9]+/
+        return re.test(weeksOnList)
       }
     },
     // https://vuejs.org/v2/guide/custom-directive.html
